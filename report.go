199e1d4775c4096065df9ce5c19952e53ac00c0f
zhangchen <zhangchen5@xiaomi.com>
2019-03-29 21:55:33 +0800

    add time duration parse

--- report.go
+++ report.go
@@ -6,7 +6,23 @@ import (
     "io"
     "log"
     "os"
+    "time"
 )
+
+type Activity struct {
+    name string
+    duration time.Duration
+    note string
+}
+
+func parseRecord(r []string) Activity {
+    var act Activity
+    act.name = r[0]
+    d := fmt.Sprintf("%sh%sm", r[1][0:2], r[1][3:])
+    act.duration, _ = time.ParseDuration(d)
+    act.note = r[4]
+    return act
+}
 
 func main() {
     f, err := os.Open(os.Args[1])
@@ -24,6 +40,10 @@ func main() {
         if err != nil {
             log.Fatal(err)
         }
-        fmt.Println(record)
+        if len(record) < 1 {
+            break
+        }
+        act := parseRecord(record)
+        fmt.Printf("%s\t%s\t%v\t%s\n", act.name, record[1], act.duration, act.note)
     }
 }
